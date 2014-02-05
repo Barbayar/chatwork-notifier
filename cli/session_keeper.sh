b0eaae7ab4a903b7d26f0fda8ee842c18196a1b8
Barbayar Dashzeveg <barbayar.dashzeveg@gmail.com>
2014-02-05 13:44:06 +0900

    added session_keeper.sh

---
+++ cli/session_keeper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# it keeps the session alive
+
+TOKEN=$(cat ../data/chatwork.token)
+FUTURE='201231' # 2020.12.31
+URL="https://kcw.kddi.ne.jp/gateway.php?cmd=get_update&_t=$TOKEN&last_id=${FUTURE}_9999999999"
+
+# sends a request every 30 minutes
+while true
+do
+    curl $URL --cookie ../data/chatwork.cookie
+    sleep 1800
+done
